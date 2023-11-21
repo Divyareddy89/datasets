ee435a8e740d68a7160fe5d6ff3a89e7945c3cbd
GDivya <132926195+Divyareddy89@users.noreply.github.com>
2023-11-21 14:48:25 -0500

    Add files via upload

---
+++ Ganesha.py
@@ -0,0 +1,18 @@
+print("Jai Ganesha")
+if(9>8):
+ print("Nine is greater than 8")
+n=float(9)
+print(n)
+p="Hi Ganesha"
+print(p)
+#This is python practice
+"""This 
+Is 
+Also
+A python
+Practice"""
+print(type(n))
+print(type(p))
+My_Favourite="SriGanesha"
+print("My favorite god is my " )
+print(My_Favourite)
